be1e7a8de9f7eae9c7bdb28f8c2060becd97ddfe
Alexander Grube <alexander.grube@cancom.de>
2022-03-30 23:30:31 +0200

    test uptime check

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"net"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
+
+type URL struct {
+	Name string `json:"name"`
+}
 
 var (
 	PORT string = ":" + os.Getenv("PORT")
@@ -12,10 +17,21 @@ var (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	r.POST("/up", func(c *gin.Context) {
+		// check if url is up
+		var url URL
+		c.BindJSON(&url)
+		_, err := net.Dial("tcp", url.Name)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"message": "down",
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"message": "up",
+			})
+		}
 	})
 	r.Run(PORT)
 }
